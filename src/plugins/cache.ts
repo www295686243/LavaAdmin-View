d3e9043dbe1fe4f3318d114d0a851a95de5e7819
wanx <wanx@app.com>
2020-08-11 18:08:44 +0800

    修改缓存前缀

--- src/plugins/cache.ts
+++ src/plugins/cache.ts
@@ -1,16 +1,16 @@
 import store from 'store2'
 
-const uniqueId = process.env.NODE_ENV === 'development' ? 'dev.lavacms/' : 'lavacms/'
+const prefix = process.env.VUE_APP_BASEURL + '/'
 
 /**
  * 调用方式参考store2插件(例如:)config.get('menuEnum')
  * config: {menuEnum}
  */
 export default {
-  user: store.namespace(uniqueId + 'user'), // 会员缓存
-  layout: store.namespace(uniqueId + 'layout'),
-  config: store.namespace(uniqueId + 'config'),
-  version: store.namespace(uniqueId + 'version'),
+  user: store.namespace(prefix + 'user'), // 会员缓存
+  layout: store.namespace(prefix + 'layout'),
+  config: store.namespace(prefix + 'config'),
+  version: store.namespace(prefix + 'version'),
   clearAll () {
     store.clearAll()
   }
