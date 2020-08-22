45bbad1de67ca037397c096e6c1cf9bf05a8db27
wanx <wanx@app.com>
2020-08-22 17:50:45 +0800

    优化图表数据

--- src/views/user/report/Service.ts
+++ src/views/user/report/Service.ts
@@ -4,6 +4,18 @@ class Service {
   index (params: { page: number }) {
     return axios.get('notify', params)
   }
+
+  createTodayData () {
+    return axios.get('chart/createTodayData')
+  }
+
+  getCurrentMonthData (date: string) {
+    return axios.get('chart/getCurrentMonthData', { date })
+  }
+
+  getCurrentYearData (date: string) {
+    return axios.get('chart/getCurrentYearData', { date })
+  }
 }
 
 export default new Service()
--- src/views/user/report/index.vue
+++ src/views/user/report/index.vue
@@ -1,5 +1,8 @@
 <template>
   <PageContainer class="view-user-report" :onLoad="handleLoad">
+    <div class="tools">
+      <ButtonSubmit :onClick="handleCreateTodayData">生成今日数据</ButtonSubmit>
+    </div>
     <el-row :gutter="40">
       <el-col :span="12"><ChartRender type="line" title="授权量"></ChartRender></el-col>
       <el-col :span="12"><ChartRender type="line" title="注册量"></ChartRender></el-col>
@@ -10,6 +13,8 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import ChartRender from '@/components/Charts/ChartRender.vue'
+import Service from './Service'
+import { getDate } from '@/plugins/tools'
 
 @Component({
   components: {
@@ -18,7 +23,19 @@ import ChartRender from '@/components/Charts/ChartRender.vue'
 })
 export default class ViewUserReport extends Vue {
   private handleLoad () {
-    return Promise.resolve()
+    return Service.getCurrentMonthData(getDate())
+  }
+
+  private handleCreateTodayData () {
+    return Service.createTodayData()
   }
 }
 </script>
+
+<style lang="scss">
+.view-user-report {
+  .tools {
+    margin-bottom: 20px;
+  }
+}
+</style>
