4a9934dbc140178af83777f4d36783027f5357e9
wanx <wanx@app.com>
2020-10-24 17:52:17 +0800

    小修复

--- src/views/info/info-delivery/Service.ts
+++ src/views/info/info-delivery/Service.ts
@@ -22,9 +22,9 @@ class Service extends BaseModalService {
       .then((res) => {
         const models = ConstService.getModelNames()
         res.data.data = res.data.data.map((item: DataItem) => {
-          const sendInfoItem = models.find((row) => row.id === item.send_info_type)
+          const sendInfoItem = models.find((row) => row.value === item.send_info_type)
           const send_type = sendInfoItem?.display_name as string
-          const receive_type = models.find((row) => row.id === item.receive_info_type)?.display_name as string
+          const receive_type = models.find((row) => row.value === item.receive_info_type)?.display_name as string
           item.type = {
             text: `${send_type}投递给${receive_type}`,
             color: sendInfoItem?.color as string
