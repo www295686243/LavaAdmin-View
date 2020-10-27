45c3c01b661109166aa2a1a2dd31dfd9746ebbb5
wanx <wanx@app.com>
2020-10-27 17:40:42 +0800

    小修复

--- src/views/operation/coupon-template/index.vue
+++ src/views/operation/coupon-template/index.vue
@@ -2,7 +2,7 @@
   <TableRender :Service="Service">
     <TableText prop="display_name" label="券名" :width="160"></TableText>
     <TableText prop="desc" label="描述" :width="300"></TableText>
-    <TableOptions prop="is_trade" label="是否可交易" :width="100" :options="ConstService.getBoolOptions()"></TableOptions>
+    <TableOptions prop="is_trade" label="是否可交易" :width="100" :options="ConstService.getBoolOptions()" :bool="true"></TableOptions>
     <TableAction :buttons="[
       { name: '编辑' }
     ]" :minWidth="160" />
